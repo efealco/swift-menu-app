efa5bbb50cc5c098528ac0ef4d3e644913306975
Efe Alço <efe.alco@cloudflight.io>
2025-05-08 01:06:00 +0300

    Initial Commit

---
+++ LittleLemonMenuApp/LittleLemonMenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonMenuAppApp.swift
+//  LittleLemonMenuApp
+//
+//  Created by Efe Alço on 8.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonMenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
